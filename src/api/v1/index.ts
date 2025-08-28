a00144f900bb85b66700ecf3fa5f10c8b433c81b
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2025-08-28 18:09:52 +0200

    fix(api): correct URL formatting in API server configuration

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -17,7 +17,7 @@ export default new API(
 		},
 		servers: [
 			{
-				url: 'https://' + PUBLIC_MAIN_DOMAIN + ' /api/v1',
+				url: 'https://' + PUBLIC_MAIN_DOMAIN + '/api/v1',
 				description: 'Production server'
 			},
 			{
